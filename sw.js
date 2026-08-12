cd7b39300465abeba8f765ea4c5ee326ca1c8666
archanirudh27-rgb <arch.anirudh27@gmail.com>
2026-08-12 21:18:55 +0530

    Implement versioned AK FinTrack PWA service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='ak-expense-v2';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+const CACHE_NAME='ak-fintrack-pwa-v3';
+const SHELL=['./','./index.html','./manifest.webmanifest','./config.js','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
