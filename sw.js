b0d06340abf9cf66ea8e27227f86230a57658428
archanirudh27-rgb <arch.anirudh27@gmail.com>
2026-08-13 15:49:01 +0530

    Refresh PWA cache for income tracker fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ak-fintrack-pwa-v11';
+const CACHE_NAME='ak-fintrack-pwa-v12';
 const SHELL=['./','./index.html','./manifest.webmanifest','./config.js','./icon-192.svg','./icon-512.svg','./settings-overlay.js','./hide-budget.js','./investment-overlay.js','./income.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
