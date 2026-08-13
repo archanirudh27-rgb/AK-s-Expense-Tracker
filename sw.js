69365a08c94eea92d707092878b5c2e6131d7252
archanirudh27-rgb <arch.anirudh27@gmail.com>
2026-08-13 10:49:12 +0530

    Refresh PWA cache for mobile settings update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ak-fintrack-pwa-v5';
+const CACHE_NAME='ak-fintrack-pwa-v6';
 const SHELL=['./','./index.html','./manifest.webmanifest','./config.js','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
