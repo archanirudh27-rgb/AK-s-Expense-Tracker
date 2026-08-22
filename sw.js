e662eaa7ff05d354539a2d88a4b25d6447065492
archanirudh27-rgb <arch.anirudh27@gmail.com>
2026-08-22 14:11:55 +0530

    Bump FinTrack PWA cache to v33

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='fintrack-pwa-v32';
-const SHELL=['./','./index.html','./manifest.webmanifest?v=32','./runtime-config.js?v=32','./fintrack-icon-192.svg?v=29','./fintrack-icon-512.svg?v=29'];
+const CACHE_NAME='fintrack-pwa-v33';
+const SHELL=['./','./index.html','./manifest.webmanifest?v=33','./runtime-config.js?v=33','./fintrack-icon-192.svg?v=29','./fintrack-icon-512.svg?v=29'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
@@ -8,7 +8,7 @@ self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
   if(url.origin!==self.location.origin)return;
   if(url.pathname.endsWith('/runtime-config.js')){
-    event.respondWith(fetch('./runtime-config.js?v=32',{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
+    event.respondWith(fetch('./runtime-config.js?v=33',{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
     return;
   }
   if(event.request.mode==='navigate'){
