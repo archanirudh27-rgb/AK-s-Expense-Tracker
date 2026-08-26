6b1605110a7b787bc0e82ff458115087a067f431
archanirudh27-rgb <arch.anirudh27@gmail.com>
2026-08-26 20:52:15 +0530

    Bump stable FinTrack PWA to v67

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fintrack-pwa-v66';
+const CACHE_NAME='fintrack-pwa-v67';
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request,{cache:'no-store'}))});
